4660ef908be60bc673e163258268b80648ecfa9f
Abdoo57 <Dfgyrddz345@gmail.com>
2025-07-24 02:52:38 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function checkPassword() {
-  const password = document.getElementById('passwordInput').value;
-  const errorMsg = document.getElementById('errorMsg');
-  if (password === '1234') {
-    window.location.href = 'gift.html';
-  } else {
-    errorMsg.textContent = '❌ الرمز غير صحيح، حاول مرة أخرى.';
-  }
-}
